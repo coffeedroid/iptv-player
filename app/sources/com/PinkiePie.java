541fbb60e0b7961cc48a426da65e989cbe74511e
Aiyden Mann <86642060+aiydenmann@users.noreply.github.com>
2021-06-29 09:38:44 +0300

    pinkiepie

---
+++ app/sources/com/PinkiePie.java
@@ -0,0 +1,14 @@
+package com;
+
+public class PinkiePie {
+    public static void DianePie() {
+    }
+
+    public static boolean DianePieNull() {
+        return false;
+    }
+
+    public static boolean DianePieOne() {
+        return true;
+    }
+}
